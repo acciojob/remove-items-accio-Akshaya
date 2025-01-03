25fac7053b59648a2884d11000a15c67c7aeace2
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-02 21:03:52 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedOption() {
+	let colorselect = document.getElementById("colorSelect");
+
+	if(colorselect.Selectedindex !== -1){
+		colorselect.remove(colorselect.Selectedindex);
+	}
+	
+}
