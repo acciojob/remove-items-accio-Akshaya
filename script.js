acd222ca6df656f26f1d9dec8595b758ccc36f7a
accio-Akshaya <akshayaganesh0910@gmail.com>
2025-01-02 21:25:40 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-function removeSelectedOption() {
+document.querySelector('input[type="button"]').addEventListener('click',()=>{
 	const colorSelect = document.getElementById("colorSelect");
+	const selectedIndex = colorSelect.selectedIndex;
 
-	if(colorSelect.SelectedIndex !== -1){
-		colorSelect.options[colorSelect.SelectedIndex].remove();
+	if(selectedIndex !==-1){
+		colorSelect.options[selectedIndex].remove();
 	}
-	
-}
+})
